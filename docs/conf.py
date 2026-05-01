aae29c6f027832b2e9cfd8de95ea0c633312fda8
acronishub <monalisa41411@hotmail.com>
2026-05-01 17:24:50 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,31 @@
-project = 'Acronis'
-author = 'Your Name'
+project = 'acronisinfo'
+author = 'acronisinfo'
 release = '1.0'
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Theme
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-html_js_files = [
-    'chatbot.js',
-]
-html_favicon = '.png'
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
+# Google & Bing Verification Meta Tags
+html_context = {
+    "meta_tags": """
+    <meta name="google-site-verification" content="Cva8KgvW-eQpRtsdf8vIcSb023IJtLJfC8PxJAlQ0mc" />
+    <meta name="msvalidate.01" content="739245F5D54BCBF40AC056DC0CBF5710" />
+    """
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://acronisinfo.readthedocs.io/en/latest/'
